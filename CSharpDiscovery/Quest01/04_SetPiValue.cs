b36d54808329facb7fdb016f4cf77e0519ca480a
Alexandre Dubois <contact@alexandredubois.com>
2021-02-22 09:48:03 +0100

    Fix wrong variable name

--- CSharpDiscovery/Quest01/04_SetPiValue.cs
+++ CSharpDiscovery/Quest01/04_SetPiValue.cs
@@ -7,7 +7,7 @@ namespace CSharpDiscovery.Quest01
     public class _04_SetPiValue
     {
         //Quest 1.4
-        //Modifie la méthode suivante pour qu'elle initialise la variable "a" reçue en paramètre avec la valeur de Pi (5 chiffres après la virgule)
+        //Modifie la méthode suivante pour qu'elle initialise la variable "pi" reçue en paramètre avec la valeur de Pi (5 chiffres après la virgule)
         public static float setPiValue(float pi)
         {
             return pi;
